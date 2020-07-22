c12188cf845e78f08297c4fa70f1f0da9229c9e3
Ansh13Patel <66872036+Ansh13Patel@users.noreply.github.com>
2020-07-22 19:02:02 +0530

    Chess Board and Player

---
+++ Chess Game/Assets/Scripts/Colourchange.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colourchange : MonoBehaviour
+{
+
+    private Vector2 position;
+    private Vector2 direction;
+    private float length;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Physics2D.Raycast(position, direction, length);
+            Physics2D.Raycast(new Vector2((Input.mousePosition).x,(Input.mousePosition).y), Vector2.zero, 0f);
+
+            this.GetComponent<SpriteRenderer>().color = Color.blue;
+        }
+        
+    }
+}
